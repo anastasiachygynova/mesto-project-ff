d7d21ea7af7d49bf5223cd757ca8ac539a319483
Anastasia Chygynova <anastasia_chygynova@mail.ru>
2025-04-07 16:59:55 +0300

    добавленны карточки и кнопка удалить

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,26 +1,26 @@
 const initialCards = [
     {
-      name: "Архыз",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+      name: "Вена. Австрия",
+      link: "https://images.unsplash.com/photo-1743102821158-314fc4c16d32?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-      name: "Челябинская область",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+      name: "Санторини. Греция",
+      link: "https://images.unsplash.com/photo-1743427533188-d15184903f63?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-      name: "Иваново",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+      name: "Амстердам. Нидерланды",
+      link: "https://images.unsplash.com/photo-1742054294226-86d430d25ae5?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
       name: "Камчатка",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
     },
     {
-      name: "Холмогорский район",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+      name: "Рим. Италия",
+      link: "https://images.unsplash.com/photo-1742161627294-2c46bbd9b7e1?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-      name: "Байкал",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+      name: "Ницца. Франция",
+      link: "https://images.unsplash.com/photo-1740896552919-6500379b85e2?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     }
 ];
--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,28 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+function createCards(cardDetails, deleteCallback) {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardItems = cardTemplate.querySelector(".card").cloneNode(true);
+  const imageCard = cardItems.querySelector(".card__image");
+  const buttonDelete = cardItems.querySelector(".card__delete-button");
+  const cardTitle = cardItems.querySelector(".card__title");
+
+  cardTitle.textContent = cardDetails.name;
+  imageCard.alt = cardDetails.name;
+  imageCard.src = cardDetails.link;
+
+  buttonDelete.addEventListener("click", (event) => {
+    deleteCallback(cardItems);
+  });
+
+  return cardItems;
+}
+function deleteCards(cardItems) {
+  cardItems.remove();
+}
+const plasesList = document.querySelector(".places__list");
+initialCards.forEach((cardDetails) => {
+  const cardItems = createCards(cardDetails, deleteCards);
+  plasesList.append(cardItems);
+});
